279f4a3c9bdfa1e14d801862050ce1f4918aa3f8
Alexandre Chataignon <alexandre@xouillet.info>
2017-05-15 04:34:09 +0200

    Many fixes after first deployment

--- nestor.py
+++ nestor.py
@@ -4,29 +4,35 @@
 import os
 import mimetypes
 
-from bottle import get, post, run, static_file, request, response, template, redirect
+from bottle import default_app, run, \
+                   get, post, \
+                   static_file, template, redirect, \
+                   request, response
 
 mimetypes.init()
+mimetypes.add_type('text/css', '.css') 
 
 #
 # Configuration
 #
 
-DATA_DIR = 'data'  # Data containing file to be served
-URL_PREFIX = ''  # Prefix for routing url, useful when reverse-proxyfing
+DATA_DIR = os.environ.get('NESTOR_DATADIR', 'data')  # Data containing file to be served
+URL_PREFIX =  os.environ.get('NESTOR_PREFIX', '')  # Prefix for routing url, useful when reverse-proxyfing
 INDEX = ['index.html']
-SECRET = 'veryverysecret'  # Secret for crypting the cookie
-BACKGROUND = ''
-PASSWORD = ''
+SECRET = 'seikaGh7eeK4satuFae0yohbnai4pieYAhH1thai'  # Secret for crypting the cookie
+BACKGROUND = os.environ.get('NESTOR_BACKGROUND', '')
+PASSWORD = os.environ.get('NESTOR_PASSWORD')
 
 #
 # Cookie content
 #
+def truncate_path(path, level):
+    return '/'.join(path.split('/')[:level])
 
 class Auths(list):
     def check(self, _path):
         for (level, path) in self:
-            if path.rsplit('/', level) == _path.rsplit('/', level):
+            if truncate_path(path, level) == truncate_path(_path, level):
                 return True 
         return False
 
@@ -39,16 +45,17 @@ def auth(path):
     password = request.forms.get('password')
     cookie = request.get_cookie("nestor", secret=SECRET) or []
     if password == PASSWORD:
+        cookie.append((0, '/'))
         response.set_cookie("nestor", cookie, secret=SECRET)
 
-    return redirect(path)
+    return redirect(URL_PREFIX+path)
 
 @get(URL_PREFIX+'<path:path>')
 def main(path):
 
     cookie = request.get_cookie("nestor", secret=SECRET)
     if not (isinstance(cookie, list) and Auths(cookie).check(path)) \
-        and path != BACKGROUND:
+        and URL_PREFIX+path != BACKGROUND:
         return template('login', background=BACKGROUND)
 
     if path == '/':
@@ -63,3 +70,5 @@ def main(path):
 
 if __name__ == '__main__':
     run(reloader=True)
+else :
+    app = application = default_app()
