920a9007a8f9fe6569fec7033bd3cd6e989869e6
Alexandre Chataignon <alexandre@xouillet.info>
2017-05-20 20:38:35 +0200

    Fixes cookie, index and random backgrounds

--- nestor.py
+++ nestor.py
@@ -3,6 +3,7 @@
 
 import os
 import mimetypes
+import random
 
 from bottle import default_app, run, \
                    get, post, \
@@ -20,7 +21,7 @@ DATA_DIR = os.environ.get('NESTOR_DATADIR', 'data')  # Data containing file to b
 URL_PREFIX =  os.environ.get('NESTOR_PREFIX', '')  # Prefix for routing url, useful when reverse-proxyfing
 INDEX = ['index.html']
 SECRET = 'seikaGh7eeK4satuFae0yohbnai4pieYAhH1thai'  # Secret for crypting the cookie
-BACKGROUND = os.environ.get('NESTOR_BACKGROUND', '')
+BACKGROUND_DIR = os.environ.get('NESTOR_BACKGROUND_DIR', None)
 PASSWORD = os.environ.get('NESTOR_PASSWORD')
 
 #
@@ -46,19 +47,22 @@ def auth(path):
     cookie = request.get_cookie("nestor", secret=SECRET) or []
     if password == PASSWORD:
         cookie.append((0, '/'))
-        response.set_cookie("nestor", cookie, secret=SECRET)
+        response.set_cookie("nestor", cookie, secret=SECRET, path="/")
 
     return redirect(URL_PREFIX+path)
 
 @get(URL_PREFIX+'<path:path>')
 def main(path):
-
     cookie = request.get_cookie("nestor", secret=SECRET)
-    if not (isinstance(cookie, list) and Auths(cookie).check(path)) \
-        and URL_PREFIX+path != BACKGROUND:
-        return template('login', background=BACKGROUND)
+    if not ((isinstance(cookie, list) and Auths(cookie).check(path)) or
+            (BACKGROUND_DIR is not None and path[1:].startswith(BACKGROUND_DIR))):
+        if BACKGROUND_DIR:
+            background=URL_PREFIX+'/'+BACKGROUND_DIR+random.choice(os.listdir(os.path.join(DATA_DIR, BACKGROUND_DIR)))
+        else:
+            background=''
+        return template('login', background=background)
 
-    if path == '/':
+    if path.endswith('/'):
         for path in INDEX:
             if os.path.exists(os.path.join(DATA_DIR, path)):
                 break
