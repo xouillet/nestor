996a25ed34951ebbb699b40a7ecb4c72f1d81cc9
Alexandre Chataignon <alexandre.git@xouillet.info>
2022-06-21 23:26:47 +0200

    Handle cookie path, updated requirements

--- nestor.py
+++ nestor.py
@@ -6,26 +6,31 @@ import hashlib
 import hmac
 import json
 import logging
+import urllib.parse
 
 from starlette.applications import Starlette
 from starlette.config import Config
 from starlette.datastructures import Secret
-from starlette.responses import RedirectResponse, Response
+from starlette.responses import RedirectResponse, Response, JSONResponse
 from starlette.routing import Route
 from starlette.templating import Jinja2Templates
 
 
 templates = Jinja2Templates(directory="views")
 
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.DEBUG)
-
 # Config
 
 config = Config(".env")
+DEBUG = config("DEBUG", default=False)
 SECRET_KEY = config("SECRET_KEY").encode("utf8")
 PASSWORD = config("PASSWORD", cast=Secret)
+ADMIN_PASSWORD = config("ADMIN_PASSWORD", cast=Secret)
 BG_URL = config("BG_URL", default=None)
+
+# Logging
+
+logger = logging.getLogger("uvicorn")
+logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
 
 #
 # Cookie content and helpers
@@ -43,40 +48,49 @@ class Auths(list):
     """
 
     def check(self, _path):
-        """" Checks that _path is allowed with current auth list """
+        """ " Checks that _path is allowed with current auth list"""
         for path in self:
+            logger.debug(f"Checking {_path} against {path}")
             if _path.startswith(path):
                 return True
         return False
 
 
-def decode_cookie(cookie):
-    """ Decode cookie content and check digest """
+def decode_authkey(authkey):
+    """Decode authkey content and check digest"""
 
-    digest, data = base64.b64decode(cookie.encode("utf8")).split(b"|", 1)
+    digest, data = base64.b64decode(authkey.encode("utf8")).split(b"|", 1)
     assert digest == hmac.new(SECRET_KEY, data, digestmod=hashlib.sha256).digest()
     return json.loads(data.decode("utf-8"))
 
 
-def encode_cookie(cookie):
-    """ Encode cookie with json inside base64 and add digest """
-    data = json.dumps(cookie).encode("utf-8")
+def encode_authkey(paths):
+    """Encode authkey with json inside base64 and add digest"""
+
+    data = json.dumps(paths).encode("utf-8")
     return base64.b64encode(
         hmac.new(SECRET_KEY, data, digestmod=hashlib.sha256).digest() + b"|" + data
     ).decode("utf8")
 
 
 def check_login(request):
-    """ Helper that returns whether cookie allows current path """
+    """Helper that returns whether cookie allows current path"""
 
     try:
-        cookie = decode_cookie(request.cookies.get("nestor"))
-        assert isinstance(cookie, list)
+        paths = decode_authkey(request.cookies.get("nestor"))
+        logger.debug(f"Found cookie with paths: {paths}")
+        assert isinstance(paths, list)
     except Exception as e:
         logger.debug(f"Unable to decode cookie : {e}")
         return False
 
-    return Auths(cookie).check(request.url.path)
+    return Auths(paths).check(request.headers['X-Original-URI'])
+
+
+class NestorCookieResponse(RedirectResponse):
+    def __init__(self, paths, redir):
+        super().__init__(url=redir, status_code=302)
+        self.set_cookie("nestor", encode_authkey(paths), path="/")
 
 
 #
@@ -84,25 +98,49 @@ def check_login(request):
 #
 
 
+async def admin(request):
+    """POST view, returns url for auth paths from json body
+    Need ADMIN_PASSWORD as Auth: header
+    Body: ['<path1>', '<path2>', ...]
+    """
+
+    if request.method == "POST":
+        assert request.headers["X-NESTOR-ADMIN"] == str(ADMIN_PASSWORD)
+
+        urls = await request.json()
+        assert isinstance(urls, list)
+        authkey = encode_authkey(urls)
+        return JSONResponse(
+            {
+                "authkey": authkey,
+                "url": f"/nestor/link?authkey={urllib.parse.quote_plus(authkey)}",
+            }
+        )
+
+
+async def link(request):
+    """GET view, with autkey as args, and optionally redir"""
+
+    assert "authkey" in request.query_params
+    paths = decode_authkey(request.query_params["authkey"])
+    return NestorCookieResponse(
+        paths=paths,
+        redir=request.query_params.get("redir", default=paths[0]),
+    )
+
+
 async def login(request):
-    """ POST view: Check for login """
+    """POST view: Check for login"""
 
     redir = None
     if request.method == "POST":
         form = await request.form()
         password = form.get("password")
         redir = form.get("next", "/")
-        try:
-            cookie = decode_cookie(request.cookies.get("nestor"))
-        except Exception:
-            cookie = []
 
         if password == str(PASSWORD):
             logging.info("Allowed access")
-            cookie.append("")
-            response = RedirectResponse(url=redir, status_code=302)
-            response.set_cookie("nestor", encode_cookie(cookie), path="/")
-            return response
+            return NestorCookieResponse(["/"], redir)  # Default is full access
 
     if check_login(request):
         return RedirectResponse(url=redir or "/", status_code=302)
@@ -113,7 +151,7 @@ async def login(request):
 
 
 async def auth(request):
-    """ Auth check view, that will be called by ngx_http_auth_request_module """
+    """Auth check view, that will be called by ngx_http_auth_request_module"""
 
     if check_login(request):
         return Response("", status_code=200)
@@ -124,6 +162,8 @@ async def auth(request):
 routes = [
     Route("/login", login, methods=["GET", "POST"]),
     Route("/auth", auth),
+    Route("/admin", admin, methods=["POST"]),
+    Route("/link", link),
 ]
 
-app = Starlette(debug=True, routes=routes)
+app = Starlette(debug=DEBUG, routes=routes)
